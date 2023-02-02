33bb1e1a5e120f212812a987e9688d0044e48a25
raphael <raphaelhaasper@gmail.com>
2023-02-02 22:06:25 +0100

    No important changes, only comment clean up

--- src/main/java/com/javalab/java_lab/JavaLabApplication.java
+++ src/main/java/com/javalab/java_lab/JavaLabApplication.java
@@ -23,12 +23,6 @@ public class JavaLabApplication {
 		log.info("Java Lab Application Started");
 	}
 
-	//TODO
-	// rename EmployeeDTO => Employee and Employee => EmployeeEntity => DONE
-	// implement ErrorMessage classes on both ApiResponse  => DONE
-	// implement CustomException class on EntityAPi => DONE
-	// Implmenet mapper from CustomException to errorMessage => DONE
-	// refactor Service and Controller to return either Employee or the CustomException => DONE
-	// refactor Ceontroller to return ResponseEntity<Employee> || ResponseEntity<ErrorMessage> => DONE
-	// finish implementing the RestClientError class to bypass WebClient and return what we want => DONE
+	// If we're reading this, this is the newest version
+
 }
